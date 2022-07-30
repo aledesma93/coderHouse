48073600a057b8d76baef3ac616d63666f2afd49
ludmiladepazos <57948463+aledesma93@users.noreply.github.com>
2022-07-30 18:22:56 -0300

    se sube desafio 3

--- clase-3/clase3.js
+++ clase-3/clase3.js
@@ -4,4 +4,12 @@
 // { id:4, nombre:'Paleta Pintura', precio:456.78 },
 // { id:5, nombre:'Reloj', precio:67.89 },
 // { id:6, nombre:'Agenda', precio:78.90 }]
-// console.log("anda")
+//  console.log("anda")
+const http= require("http");
+
+const server = http.createServer((peticion, respuesta)=>{
+    respuesta.end("hola mundo")
+});
+const connectedServer = server.listen(8080,()=>{
+    console.log("servidor http escuchando en el puerto: "+connectedServer.address().port)
+})
