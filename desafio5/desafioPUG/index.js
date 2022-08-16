beba19120a07260ee49baa678bf0e99fce7819e9
ludmiladepazos <57948463+aledesma93@users.noreply.github.com>
2022-08-16 16:53:54 -0300

    subo PUG

---
+++ desafio5/desafioPUG/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+let productsHC = [
+  { id: 1, title: 'nike ball', price: 101, thumbnail: 'http://localhost:8080/public/nike-ball.jpg' },
+  { id: 2, title: 'nike shoes', price: 102, thumbnail: 'http://localhost:8080/public/nike-shoes.jpg' },
+  { id: 3, title: 'adidas shoes', price: 103, thumbnail: 'http://localhost:8080/public/adidas-shoes.jpg' },
+];
+
+app.get('/hello', (req, res) => {
+  res.render('hello.pug', { msg: 'Esto Funca' });
+});
+
+app.get('/products', (req, res) => {
+  res.render('products.pug', { title: 'listado de perros', products: productsHC });
+});
+
+app.get('/products/:id', (req, res) => {
+  let{id}=req.params;
+  const productoEncontrado=productsHC.find(e=>e.id==id)
+  if (productoEncontrado){
+    res.render('unproducto.pug', { title: 'Un Producto Encontrado', productoEncontrado });
+
+  }else{
+    res.render('noEncontrado.pug', { title: 'Producto No Encontrado', productoEncontrado });
+
+  }
+});
+
+app.get('/form', (req, res) => {
+  res.render('form.pug', { title: 'Ingrese su Producto'});
+});
+
+app.post('/products',(req,res)=>{
+  console.log("hace algo")
+  const {body}=req;
+  console.log(body);
+  return res.redirect(`/form`);
+})
