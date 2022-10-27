3a73e8841d17ac249540242426e29630ce3e5cab
ludmiladepazos <57948463+aledesma93@users.noreply.github.com>
2022-10-27 18:46:49 -0300

    readme desafio14

---
+++ nginx-main/NginxNode/public/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.enable("trust proxy");
+//app.use(express.static('public'))
+const PORT = parseInt(process.argv[2]) || 8080;
+app.get("/datos", (req, res) => {
+  console.log(`port: ${PORT} -> Fyh: ${Date.now()}`);
+  res.send(`Servidor express <span style="color:blueviolet;">(Nginx)</span> en ${PORT} - 
+    <b>PID ${process.pid}</b> - ${new Date().toLocaleString()}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`);
+});
+
+//(dentro de la carpeta NginxNode/public donde esta el server.js)
+//pm2 start server.js --name="Server2" --watch -i max -- 8082
+//pm2 start server.js --name="Server1" --watch -- 8081
+
+//(dentro de la carpeta base de to el proyecto donde esta nginx.exe)
+//nginx
